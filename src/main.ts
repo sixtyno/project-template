38c1b73b2c4b18e21705e30e5e21b494a06509b6
ablazejowski <andrzej@sixty.no>
2022-03-03 09:12:49 +0100

    fix(readme): modify readme, fix spelling , modified starting page (#5)
    * add some readme
    
    * added more text to readme, modified starting page

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,11 @@
-import './style.css'
+import "./style.css";
 
-const app = document.querySelector<HTMLDivElement>('#app')!
+const app = document.querySelector<HTMLDivElement>("#app")!;
 
 app.innerHTML = `
-  <h1>Hello Vite!</h1>
-  <a href="https://vitejs.dev/guide/features.html" target="_blank">Documentation</a>
-`
+  <h1>Hello Sixty Developer!</h1>
+  <a href="https://vitejs.dev/guide/features.html" target="_blank">Documentation for vite</a><br>
+  <a href="https://www.typescriptlang.org/docs/" target="_blank">Documentation for TypeScript</a><br>
+  <a href="https://github.com/semantic-release/semantic-release" target="_blank">Documentation for Semantic release</a><br>
+  <a href="https://circleci.com/docs/" target="_blank">Documentation for CircleCi</a><br>
+`;
